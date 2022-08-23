679a0562f7566721b50d552776ab837bf62f73ca
vasanth2030 <vasanths2030@gmail.com>
2022-08-23 21:20:30 +0530

    day7

---
+++ Java code/oops/Animal.java
@@ -0,0 +1,34 @@
+package oops;
+
+public class Animal {
+	private String name;
+	private String favoriteFood;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFavoriteFood() {
+		return favoriteFood;
+	}
+	public void setFavoriteFood(String favoriteFood) {
+		this.favoriteFood = favoriteFood;
+	}
+	
+	Animal(String name, String favoriteFood)
+	{
+		this.name=name;
+		this.favoriteFood=favoriteFood;
+	}
+	public void eat()
+	{
+		System.out.println("Eating");
+	}
+
+	public void sleep()
+	{
+		System.out.println("Sleeping");
+	}
+}
---
+++ Java code/oops/Main.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class Main {
+	public static void main(String[] args) {
+		Animal dog=new Animal("Luffy","Chicken");
+		
+		System.out.println(dog.getName());
+		System.out.println(dog.getFavoriteFood());
+	}
+}
