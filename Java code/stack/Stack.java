be651cf7a7d09b58762aed591ab808479be2a7a3
vasanth2030 <vasanths2030@gmail.com>
2022-08-31 21:11:05 +0530

    day12

---
+++ Java code/stack/Stack.java
@@ -0,0 +1,54 @@
+package stack;
+
+public class Stack {
+	int arr[];
+	int capacity;
+	int top;
+	
+	Stack(int capacity)
+	{
+		this.capacity=capacity;
+		arr=new int[capacity];
+		top=-1;
+	}
+
+	public void push(int value)
+	{
+		if(top==capacity-1)
+		{
+			System.out.println("Stack is full");
+			return;
+		}
+//		top++;
+		arr[++top]=value;
+	}
+	
+	public void pop()
+	{
+		if(top==-1)
+		{
+			System.out.println("Stack is empty");
+			return;
+		}
+		top--;
+	}
+	
+	public int peek()
+	{
+		if(top==-1)
+		{
+			System.out.println("Stack is empty");
+			return -1;
+		}
+		return arr[top];
+	}
+	
+	public void printStack()
+	{
+		for(int i=top;i>=0;i--)
+		{
+			System.out.print(arr[i]+" <-- ");
+		}
+		System.out.println();
+	}
+}
